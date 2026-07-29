b2613398ab4e6920ae868389fa201760e6908c09
J Dinesh Reddy <dinesh.reddy@juspay.in>
2026-07-29 17:09:00 +0530

    change to nixpkgs unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS LXC Container Builder for Proxmox";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
   nixConfig = {
